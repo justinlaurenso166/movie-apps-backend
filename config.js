ce9879f7162374bf6d546ee37afd9bc6e77802b1
JustinLaurenso <justin.laurenso166@gmail.com>
2022-04-19 20:55:15 +0700

    push

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     db: {
-        host: "bx7zfymkub8ccmer5lfa-mysql.services.clever-cloud.com",
-        user: "uf1xrxwaapixvhzb",
-        password: "28XN8U6hjM9F6KW3HjJL",
-        database: "bx7zfymkub8ccmer5lfa",
+        host: "localhost",
+        user: "id18806896_justin123",
+        password: "yLfXb]cRxKh_7+R_",
+        database: "id18806896_movies",
     }
 }
 
